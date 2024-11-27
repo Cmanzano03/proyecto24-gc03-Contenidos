e0a40e72838a4e00f7aaf41fbc3adbfda51d134d
Carlos Manzano Izquierdo <cmanzanoo@alumnos.unex.es>
2024-11-27 11:46:01 +0100

    Modificado codigo de la excepción para tratar de evitar issues en la clase con SonarCloud

--- src/main/java/org/openapitools/api/ApiUtil.java
+++ src/main/java/org/openapitools/api/ApiUtil.java
@@ -7,18 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ApiUtil {
-public static void setExampleResponse(NativeWebRequest req, String contentType, String example) {
-    try {
-        HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
-        if (res != null) {
-            res.setCharacterEncoding("UTF-8");
-            res.addHeader("Content-Type", contentType);
-            res.getWriter().print(example);
-        } else {
-            throw new HttpServletResponseException("HttpServletResponse is null");
+    public static void setExampleResponse(NativeWebRequest req, String contentType, String example) {
+        try {
+            HttpServletResponse res = req.getNativeResponse(HttpServletResponse.class);
+            if (res != null) {
+                res.setCharacterEncoding("UTF-8");
+                res.addHeader("Content-Type", contentType);
+                res.getWriter().print(example);
+            } else {
+                throw new HttpServletResponseException("HttpServletResponse is null");
+            }
+        } catch (IOException e) {
+            throw new HttpServletResponseException("Failed to write example response to HttpServletResponse");
         }
-    } catch (IOException e) {
-        throw new RuntimeException(e);
     }
-}
 }
