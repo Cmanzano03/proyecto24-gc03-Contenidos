60ffd22850601d233707f5d21200dd8e1313f3da
Carlos Manzano Izquierdo <cmanzanoo@alumnos.unex.es>
2024-12-04 14:12:29 +0100

    SCRUM-107 SUBT 48. Añadir tipo de excepción personalizada en APIUtil - Corregido el manejo de excepciones en la clase ApiUtil, añadiendo una excepción personalizada y manejándola en la clase mencionada.

---
+++ src/main/java/org/openapitools/exceptions/HttpServletResponseException.java
@@ -0,0 +1,7 @@
+package org.openapitools.exceptions;
+
+public class HttpServletResponseException extends RuntimeException {
+    public HttpServletResponseException(String message) {
+        super(message);
+    }
+}
